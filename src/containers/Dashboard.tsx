cd1ffca2444398e5ed3b8524e7a612113b1adc9d
joemerdizon <joemer.dizon@gmail.com>
2022-04-09 21:13:30 +0800

    Initial Commit

---
+++ src/containers/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const Dashboard = () => {
+    return (
+        <div id="sidebar-menu" className="main_menu_side hidden-print main_menu">
+            <div className="menu_section">              
+            </div>            
+        </div>
+    )
+}

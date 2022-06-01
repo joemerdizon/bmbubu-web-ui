db6bd4b34b3b2072815b6d12ace4bee9b1a0e944
joemerdizon <joemer.dizon@gmail.com>
2022-06-01 10:56:13 +0800

    task: added enum and ts

---
+++ src/constants/tms.ts
@@ -0,0 +1,3 @@
+export const REPORT_STATUS_SAVED : string = "Saved";
+export const REPORT_STATUS_PUBLISHED : string = "Published";
+export const REPORT_STATUS_SENT_FOR_APPROVAL : string = "Sent for Approval";
